71f6ef754701f4eeff0d7e0b88ad97a0c6e2c0d8
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-07-17 12:43:53 -0400

    Task CRUD ready

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -3,25 +3,87 @@ import TaskModel from "../models/Task";
 
 export const getTasks = async (req: Request, res: Response) => {
   try {
-    const response = await TaskModel.find({});
-    res.json(response);
+    const tasks = await TaskModel.find({});
+    return res.status(200).json({
+      data: tasks,
+    });
   } catch (error) {
-    console.log(error);
+    return res.status(500).json({
+      message: "Error getting tasks.",
+    });
   }
 };
 
 export const getTask = async (req: Request, res: Response) => {
-  res.json("getting a task");
+  try {
+    const taskId = req.params.id;
+    const task = await TaskModel.findById(taskId);
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+    return res.status(200).json({
+      data: task,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error getting task",
+    });
+  }
 };
 
 export const createTask = async (req: Request, res: Response) => {
-  res.json("creating a task");
+  try {
+    const { name, description, done } = req.body;
+    const createdTask = await TaskModel.create({ name, description, done });
+    return res.status(200).json({
+      data: createdTask,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error creating task.",
+    });
+  }
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
-  res.json("deleting a task");
+  try {
+    const taskId = req.params.id;
+    const task = await TaskModel.findById(taskId);
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found.",
+      });
+    }
+    await task.delete();
+    res.status(200).json({
+      data: task,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error deleting task.",
+    });
+  }
 };
 
 export const updateTask = async (req: Request, res: Response) => {
-  res.json("updating a task");
+  try {
+    const taskId = req.params.id;
+    const updatedTask = await TaskModel.findByIdAndUpdate(taskId, req.body, {
+      new: true,
+    });
+    if (!updatedTask) {
+      return res.status(404).json({
+        message: "Task not found.",
+      });
+    }
+    res.status(200).json({
+      data: updatedTask,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error updating task.",
+    });
+  }
 };
--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,9 @@
 import { connect } from "mongoose";
+import { MONGODB_URI } from "./config";
 
 (async () => {
   try {
-    await connect(
-      "mongodb+srv://chicha:claudio123@node-mongodb-heroku.n97zj.mongodb.net/?retryWrites=true&w=majority"
-    );
+    await connect(MONGODB_URI);
     console.log(`Database connected`);
   } catch (error) {
     console.error("Error to connect the database: ", error);
